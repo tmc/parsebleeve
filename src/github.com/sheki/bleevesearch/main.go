ae6ff1b112f86386a85cb14fc64602de11fb684e
abhishekk <abhishek@parse.com>
2015-07-09 14:31:26 -0600

    parse bleeve

--- src/github.com/sheki/bleevesearch/main.go
+++ src/github.com/sheki/bleevesearch/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/blevesearch/bleve"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +18,34 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", mux)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", hello)
+	// http.ListenAndServe(":8000", mux)
+	bleeveMain()
+}
+
+func bleeveMain() {
+
+	// open a new index
+	index, err := bleve.Open("example.bleve")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	data := struct {
+		Name string
+	}{
+		Name: "text",
+	}
+
+	// search for some text
+	query := bleve.NewMatchQuery("text")
+	search := bleve.NewSearchRequest(query)
+	searchResults, err := index.Search(search)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(searchResults)
 }
