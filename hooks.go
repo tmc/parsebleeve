d353317758dc79c5a633053df32554cb3c9562a1
Travis Cline <travis.cline@gmail.com>
2015-07-10 11:08:54 -0600

    Fix trigger creation

--- hooks.go
+++ hooks.go
@@ -3,9 +3,19 @@ package parsesearch
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/tmc/parse"
 )
+
+func squelchAlreadyExists(err error) error {
+	if err, ok := err.(*parse.Error); ok {
+		if strings.Contains(err.Message, "already exists") {
+			return nil
+		}
+	}
+	return err
+}
 
 // RegisterHooks auto-registers the search service with a Parse Application.
 func (i *Indexer) RegisterHooks(prefix string, className string) error {
@@ -20,14 +30,16 @@ func (i *Indexer) RegisterHooks(prefix string, className string) error {
 		FunctionName: prefix + "search",
 		URL:          os.Getenv("URL") + "/search",
 	})
+	err = squelchAlreadyExists(err)
 	if err != nil {
 		return err
 	}
 	err = c.CreateTriggerFunction(&parse.TriggerFunction{
-		ClassName:    className,
-		FunctionName: "afterSave",
-		URL:          os.Getenv("URL") + "/index",
+		ClassName:   className,
+		TriggerName: "afterSave",
+		URL:         os.Getenv("URL") + "/index",
 	})
+	err = squelchAlreadyExists(err)
 	if err != nil {
 		return err
 	}
